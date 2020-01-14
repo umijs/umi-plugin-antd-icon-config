516b2dd03794f1d4174e4772738be88f415de34c
chenshuai2144 <qixian.cs@outlook.com>
2020-01-14 12:09:16 +0800

    codestyle: add non-empty validation

--- src/app.ts
+++ src/app.ts
@@ -14,13 +14,25 @@ export interface MenuDataItem {
   [key: string]: any;
 }
 
+function toHump(name: string) {
+  return name.replace(/\-(\w)/g, function(all, letter) {
+    return letter.toUpperCase();
+  });
+}
+
 function formatter(data: MenuDataItem[]): MenuDataItem[] {
   data.forEach((item = { path: '/' }) => {
     if (item.icon) {
       const { icon } = item;
-      const v4IconName = icon.replace(icon[0], icon[0].toUpperCase());
+      const v4IconName = toHump(icon.replace(icon[0], icon[0].toUpperCase()));
       const NewIcon = allIcons[icon] || allIcons[`${v4IconName}Outlined`];
-      item.icon = react.createElement(NewIcon);
+      if (NewIcon) {
+        try {
+          item.icon = react.createElement(NewIcon);
+        } catch (error) {
+          console.log(error);
+        }
+      }
     }
     if (item.routes || item.children) {
       const children = formatter(item.routes || item.children);
