d8751ccc6464d570621decfcd6bec6d9fcbd54ad
chenshuai2144 <qixian.cs@outlook.com>
2020-01-14 10:46:35 +0800

    Seems to be fine

--- example/.umirc.ts
+++ example/.umirc.ts
@@ -2,6 +2,6 @@ import { join } from 'path';
 import { IConfig } from 'umi-types';
 
 export default {
-  routes: [{ path: '/', icon: 'name', component: './index' }],
+  routes: [{ path: '/', icon: 'question', component: './index' }],
   plugins: [join(__dirname, '..', require('../package').main || 'index.js')],
 } as IConfig;
--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import styles from './index.css';
 
 export default function(props) {
+  console.log();
+  const Icon = props.newRoute[0].icon;
   return (
     <div className={styles.normal}>
       <h1>Page index</h1>
+      <Icon></Icon>
     </div>
   );
 }
