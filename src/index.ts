f76adfee0e39b56586aa83b15029eb2ae93895fb
chenshuai2144 <qixian.cs@outlook.com>
2020-02-26 19:10:12 +0800

    💥 feat: support umi 3.x

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { IApi } from 'umi-types';
+import { IApi } from 'umi';
 
 export default function(api: IApi, options) {
-  api.addRuntimePlugin(require.resolve('./app.js'));
+  api.addRuntimePlugin(() => require.resolve('./app.js'));
 }
